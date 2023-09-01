964a404f20abb0e5a9af4bc53ffe58316987eecd
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-09-01 18:45:45 +0530

    update background design

--- public/src/index.js
+++ public/src/index.js
@@ -2,21 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import BG from "./assets/bg.jpg";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div
-    style={{
-      background: `url(${BG})`,
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-    }}
-  >
-    <App />
-  </div>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
